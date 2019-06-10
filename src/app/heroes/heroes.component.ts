0ebd848b54780a41007e0c942b7e7a8b8a6bdcdd
Del Piano Federico <f.delpiano@reply.it>
2019-06-10 09:46:23 +0200

    update

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,13 +10,8 @@ import {HeroService} from '../hero.service';
 export class HeroesComponent implements OnInit {
 
   heroes: Hero[];
-  selectedHero: Hero
+
   constructor(private heroService: HeroService) { }
-
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes()
@@ -24,6 +19,21 @@ export class HeroesComponent implements OnInit {
   }
   ngOnInit() {
     this.getHeroes();
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {return; }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
 }
